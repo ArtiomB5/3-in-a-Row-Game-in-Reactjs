8a342cefe4c833c67757e76cd0b253be3654c3ce
ArtiomB5 <artiomb5@yandex.by>
2022-06-12 15:59:06 +0600

    OST info added

--- src/Components/Modal/index.tsx
+++ src/Components/Modal/index.tsx
@@ -11,6 +11,8 @@ export const Modal = () => {
           <ModalCloseStyled onClick={() => setVisibility(false)}>&times;</ModalCloseStyled>
           <p>1. Click to select the gem you want to move.</p>
           <p>2. Click to select the gem where you want to place the gem from step 1.</p>
+          <hr/>
+          <p>OST: Mystery Orientale by  FSM Team</p>
         </ModalContentStyled>
       </ModalStyled>
     </div>
